3a788c32788282856fd53febcafff5452c2186f8
SaiChandra2896 <saichandra2896@gmail.com>
2019-04-17 13:41:19 +0530

    writing forecast function and exporing it

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,22 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/41609600355879151ce9e9d95f3d72ea/37.8267,-122.4233';
+const forecast = (latitude,longitude,callback) =>{
+const url = 'https://api.darksky.net/forecast/41609600355879151ce9e9d95f3d72ea/'+encodeURIComponent(latitude)+','+encodeURIComponent(longitude);
 
 request({url,json: true},(err,res) =>{
     if(err){
-        console.log('Unable to connect to server!!',err)
+        callback('Unable to connect to server!',undefined);
     }
     else if(res.body.error){
-        console.log('Invalid Location!')
+        callback('Invalid location!!', undefined);
     }
     else{
-        console.log('Success!!', res)
+        callback(undefined, res.body.daily.data[0].summary+' Its currently '+res.body.currently.temperature+' degrees out. There is a '+res.body.currently.precipProbability+'% chance of rain');
     }
-})
+});
+}
+
+forecast(17.366,78.466,(err,result) =>{
+     console.log('Error',err);
+     console.log('Result',result);
+});
