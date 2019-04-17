9b8912e368fc01fdc98ac9f0d17bc602f291161c
SaiChandra2896 <saichandra2896@gmail.com>
2019-04-17 12:01:23 +0530

    routes to handle bad requests

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 8080;
+
 app.get('',(req,res) =>{
     res.send('My weather Project');
 });
@@ -17,6 +19,22 @@ app.get('/weather',(req,res) =>{
     res.send('route to handle weather');
 });
 
-app.listen(8080,() =>{
+app.get('*',(req,res) =>{
+    res.send('error');
+});
+
+app.get('/help/*',(req,res) =>{
+    res.send('error');
+});
+
+app.get('/about/*',(req,res) =>{
+    res.send('error');
+});
+
+app.get('/weather/*',(req,res) =>{
+    res.send('error');
+});
+
+app.listen(port,() =>{
     console.log('Server is up on port 8080');
-})
+});
