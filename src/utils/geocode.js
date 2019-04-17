6875b733796540362fe0610b219bc3c712e549d9
SaiChandra2896 <saichandra2896@gmail.com>
2019-04-17 12:55:15 +0530

    writing geocode function and exporting it

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,17 +1,29 @@
 const request = require('request');
 
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2FpY2hhbmRyYSIsImEiOiJjanU2cGQ4NWkxNXB1M3lzN2xsZW9vaGJwIn0.HBvvSyWUeFR9tkY_lOemLQ';
+const geocode = (address,callback) =>{
+
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token=pk.eyJ1Ijoic2FpY2hhbmRyYSIsImEiOiJjanU2cGQ4NWkxNXB1M3lzN2xsZW9vaGJwIn0.HBvvSyWUeFR9tkY_lOemLQ';
 
    request({url, json: true},(err,res) =>{
        if(err){
-           console.log('Unamble to connect to server',err)
+           callback('Unable to connect to server!',undefined);
        }
        else if(res.body.features.length === 0){
-           console.log('Unable to fetch location')
+           //console.log('Unable to fetch location');
+           callback('Unable to fetch location. Try New Location',undefined);
        }
        else{
            //console.log(res)
-           console.log('latitude:'+res.body.features[0].center[1])
-           console.log('logitude:'+res.body.features[0].center[0])
+          callback(undefined,{
+              latitude: res.body.features[0].center[1],
+              longitude: res.body.features[0].center[0],
+              location: res.body.features[0].place_name
+          });
        }
-   })
+   });
+}
+
+geocode('hyderabad',(err,result) =>{
+   console.log('error',err)
+   console.log('result',result)
+});
