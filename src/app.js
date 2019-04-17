15726d72ed2608f53ae76baf3907e0abf46adb3c
SaiChandra2896 <saichandra2896@gmail.com>
2019-04-17 21:34:17 +0530

    added views to render and added the app.js logic to the weather route to fetch weather

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const hbs = require('hbs');
 const path = require('path');
 const express = require('express');
 const app = express();
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 //change default path to __dirname to what we wanted or define paths for express config
 const viewpath = path.join(__dirname,'../templates/views');
@@ -11,26 +13,53 @@ const publicpath = path.join(__dirname,'../public');
 //setting view engine to handlebars
 app.set('view engine','hbs');
 app.set('views',viewpath);
-hbs.registerPartials(partialspath)
+hbs.registerPartials(partialspath);
+
+app.use(express.static(publicpath));
 
 
 
 const port = process.env.PORT || 8080;
 
 app.get('',(req,res) =>{
-    res.send('My weather Project');
+   res.render('index',{
+       title: 'Weather',
+       name: 'Sai Chandra'
+   });
 });
 
 app.get('/about',(req,res) =>{
-    res.send('about page');
+    res.render('about',{
+        title: 'Weather',
+        name: 'Sai Chandra'
+    });
 });
 
 app.get('/help',(req,res) =>{
-    res.send('help page');
+    res.render('help',{
+        title: 'Weather',
+        name: 'Sai Chandra'
+    });
 });
 
 app.get('/weather',(req,res) =>{
-    res.send('route to handle weather');
+    geocode(req.query.address,(err,result) =>{
+        if(err){
+           return console.log(err);
+        }
+        forecast(result.latitude,result.longitude,(err,forecastresult) =>{
+            if(err){
+                return res.send({
+                    error: 'Unable to fetch forecastdata'
+                    
+                });
+            }
+            res.send({
+                address: req.query.address,
+                forecast: forecastdata,
+                location: result.location
+        });
+     });
 });
 
 app.get('*',(req,res) =>{
@@ -38,15 +67,24 @@ app.get('*',(req,res) =>{
 });
 
 app.get('/help/*',(req,res) =>{
-    res.send('error');
+   res.render('error',{
+       title: 'Help article not found',
+       name: 'Sai Chandra'
+   });
 });
 
 app.get('/about/*',(req,res) =>{
-    res.send('error');
+    res.render('error',{
+        title: 'About article not found',
+        name: 'Sai Chandra'
+    });
 });
 
 app.get('/weather/*',(req,res) =>{
-    res.send('error');
+    res.render('error',{
+        title: 'Weather article not found',
+        name: 'Sai Chandra'
+    });
 });
 
 app.listen(port,() =>{
