b3447c3407eadfb0877a448bfe08590e49b67b52
SaiChandra2896 <saichandra2896@gmail.com>
2019-04-17 14:20:51 +0530

    error handling in utils/app.js and added functionality to read the location from the terminal

--- src/utils/app.js
+++ src/utils/app.js
@@ -1,11 +1,18 @@
 const geocode = require('./geocode');
 const forecast = require('./forecast');
 
-geocode('hyderabad',(err,result) =>{
-    console.log('error',err)
-    console.log('result',result)
-    forecast(result.latitude,result.longitude,(err,result) =>{
-        console.log('Error',err);
-        console.log('Result',result);
+console.log(process.argv);
+const address = process.argv[2];
+
+geocode(address,(err,result) =>{
+    if(err){
+       return console.log(err);
+    }
+    forecast(result.latitude,result.longitude,(err,forecastresult) =>{
+        if(err){
+            return console.log(err);
+        }
+        console.log(result.location);
+        console.log(forecastresult);
     });
  });
